de9eacc5ac7f5bde6296a19767f4d382db1fc905
Naresh Madiraju <naresh.johnny@gmail.com>
2018-06-19 15:20:10 -0400

    simple web

--- simpleweb.py
+++ simpleweb.py
@@ -18,4 +18,4 @@ def name():
     #return jsonify({'ip': request.environ['REMOTE_ADDR']}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=5000)
