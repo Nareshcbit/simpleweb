a338ef15a165f983a1bb979eae1aa88c003d7a9d
Naresh Madiraju <naresh.johnny@gmail.com>
2018-06-19 14:36:11 -0400

    simple web

--- simpleweb.py
+++ simpleweb.py
@@ -5,7 +5,9 @@ app = Flask(__name__)
 
 @app.route('/ip', methods=['GET'])
 def name():
-    return request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+    client_ip=request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+    server_ip=request.host.split(':')[0]
+    return server_ip + " : " + client_ip
     #return jsonify({'ip': request.remote_addr}), 200
     #return jsonify({'ip': request.environ['REMOTE_ADDR']}), 200
 
